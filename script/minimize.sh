d13c20f034bbf3538d068821b716eb00a10d416d
Martin Ellis <martin@ellis.name>
2014-12-10 08:07:40 +0000

    Tidy up package installation.

--- script/minimize.sh
+++ script/minimize.sh
@@ -31,7 +31,7 @@ apt-get -y autoremove --purge
 echo "==> Removing APT lists"
 find /var/lib/apt -depth -name extended_states -prune -o -type f -delete
 echo "==> Removing caches"
-find /var/cache -type f -exec rm -rf {} \;
+find /var/cache -type f -delete
 
 # Make sure Udev doesn't block our network
 # http://6.ptmc.org/?p=164
@@ -44,10 +44,6 @@ rm -f /var/lib/dhcp/*
 
 echo "==> Cleaning up tmp"
 rm -rf /tmp/*
-
-# Cleanup apt cache
-apt-get -y autoremove --purge
-apt-get -y clean
 
 # Remove Bash history
 unset HISTFILE
