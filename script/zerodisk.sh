6967d382be119de8eab9e32b07359ab3eb67aaea
Martin Ellis <martin@ellis.name>
2014-12-11 08:23:10 +0000

    Simplify wiping free disk space.

--- script/zerodisk.sh
+++ script/zerodisk.sh
@@ -2,18 +2,14 @@
 # Whiteout root
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
 let count--
-dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
-rm /tmp/whitespace
+dd if=/dev/zero of=/tmp/zero bs=1024 count=$count
+rm /tmp/zero
 
 # Whiteout /boot
 count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
 let count--
-dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
-rm /boot/whitespace
-
-# Zero out the free space to save space in the final image
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+dd if=/dev/zero of=/boot/zero bs=1024 count=$count
+rm /boot/zero
 
 # Make sure we wait until all the data is written to disk, otherwise
 # Packer might quit too early
