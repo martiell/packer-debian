fad7c5b4e927c044f06deac38b2996dc43c62a79
Martin Ellis <martin@ellis.name>
2015-02-07 00:35:33 +0000

    Clean swap space in zerodisk.sh.

--- script/zerodisk.sh
+++ script/zerodisk.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -eux
-# Whiteout root
-count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+echo "Zero root partition."
+count=$(df --sync -kP / | awk -F ' ' '{if (NR==2) print $4}')
 let count--
 dd if=/dev/zero of=/tmp/zero bs=1024 count=$count
 rm /tmp/zero
 
-# Whiteout /boot
-count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+echo "Zero /boot partition."
+count=$(df --sync -kP /boot | awk -F ' ' '{if (NR==2) print $4}')
 let count--
 dd if=/dev/zero of=/boot/zero bs=1024 count=$count
 rm /boot/zero
 
+echo "Zero swap space."
+for d in $(awk '$3 ~ /swap/ {print $1}' /etc/fstab ); do
+  swapoff $d
+  uuid=$(swaplabel $d | awk '/UUID:/{print $2}')
+  label=$(swaplabel $d | awk '/LABEL:/{print $2}')
+  sectors=$(blockdev --getsize /dev/mapper/vagrant--vg-swap_1)
+  count=$(( $sectors / 2 ))
+  dd if=/dev/zero of=$d bs=1024 count=$count
+  mkswap -U "$uuid" -L "$label" $d
+done
+
 # Make sure we wait until all the data is written to disk, otherwise
 # Packer might quit too early
+echo "Sync."
 sync
+echo "Synced."
